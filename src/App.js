45e3a462935f918bff0992603969edd04ea15ae9
otatsuya <tao754@ensign.edu>
2024-10-07 15:48:03 -0600

    Added to home

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './Components/Header.js';
 // import Header from './Components/theme/Header/Header.js';
 import Home from './Components/Home.js';
 import Navbar from './Components/Navbar.js';
+import Footer from './Components/footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header /> {/* Include the Header component here */}
       <Home /> {/* Include the Home component here */}
+      <Footer />
     </div>
   );
 }
