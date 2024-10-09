1c14ab7825df2cd60c952fff0d9e1bea12d422e8
juan <jup017@ensign.edu>
2024-10-09 11:14:12 -0600

    language/video

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -6,7 +6,7 @@ function VideoPlayer() {
             <iframe
                 width="100%"
                 height="650"
-                src="https://www.youtube.com/embed/QlKOcyf915M?si=XDJOWw9OVvF2osoz&autoplay=1&mute=1"
+                src="https://www.youtube.com/embed/QlKOcyf915M?si=XDJOWw9OVvF2osoz&autoplay=1&mute=1&loop=1&playlist=QlKOcyf915M"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
