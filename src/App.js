bb0c2d7009567d958b00b477020ae519078cac7f
angelicarood <leo18007@byui.edu>
2024-10-14 17:21:24 -0600

    [KAN-26] implement dark mode to  homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import './App.css';
 import Header from './Components/Header.js';
-// import Header from './Components/theme/Header/Header.js';
-//import Home from './Components/Home.js';
-//import Navbar from './Components/Navbar.js';
-//import Footer from './Components/Footer.js';
-//import NewsletterSignup from './Components/NewsletterSignup.js';
 import HomePage from './Components/HomePage.js';
 import FooterWeb from './Components/FooterWeb.js';
 import Shop from './Components/Shop.js';
@@ -33,18 +28,6 @@ function App() {
 export default App;
 
 
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Header /> {/* Include the Header component here */}
-//         <Routes> {/* Corrected opening tag */}
-//           <Route path="/shop" element={<Shop />} /> {/* Shop page */}
-//           <Route path="/" element={<Home />} /> {/* Home page */}
-//           <Route path="/payment-success" element={<PaymentSuccess />} /> {/* Add this route */}
-//         </Routes> {/* Corrected closing tag */}
-//         <NewsletterSignup />
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
+
+
+
